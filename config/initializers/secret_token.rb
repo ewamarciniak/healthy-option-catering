4b6282863e56b3c0eb8a9390eda34e4844ff83c7
Ewa Marciniak <emarciniak78@gmail.com>
2012-07-02 17:54:13 +0100

    Setting a new application and adding Products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthyOptionCatering::Application.config.secret_token = '811a4b32c5a87e6c6aa1ce6725a2681c160cdc68f0dcb2620ccc03c46751b7a461ee8c4ffa97cf2d6a7439333e0ed8133210f0918f5a5e548b8e47001cce658d'
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Product.create(:title =>'Warm potato salad with shallot dressing', :description => 'German-style potato salad is tangy with shallot and white wine vinegar instead of mayonnaise', :image_url => '/images/chargrilledcourgette.jpg', :price => '5.79')
+Product.create(:title =>'Sea bream with a courgette salad with fresh mint and rocket', :description => 'Mediterranean flavours pack in punch', :image_url => '/images/fresh_pea_broad_bean.jpg', :price => '8.99')
+Product.create(:title =>'Chargrilled courgette with basil, mint, chilli and lemon', :description => 'Served at room temperature as a summery salad', :image_url => '/images/piripiriprawns.jpg', :price => '4.99')
+Product.create(:title =>'Piri-piri prawns and harissa couscous', :description => 'Delicious mix of exotic flavours', :image_url => '/images/sea_bream_with_courgette.jpg', :price => '9.39')
+Product.create(:title =>'Fresh pea, broad bean and ham hock salad', :description => 'Delicious ham hock salad recipe', :image_url => '/images/warmpotatosalad.jpg', :price => '4.99')
